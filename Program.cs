b3590d25db1a8d0354c9974c9304d80dfd3619e0
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-11 14:44:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+Виды ТС для реализации:
+    Ступа Бабы Яги;
+    Метла;
+    Сапоги-скороходы;
+    Карета-тыква;
+    Ковер-самолет;
+    Избушка на курьих ножках;
+    Кентавр;
+    Летучий корабль.
+*/
+
+namespace LR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
