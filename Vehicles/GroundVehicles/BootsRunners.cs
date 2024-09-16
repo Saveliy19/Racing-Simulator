5b0b51aefdba5599d07a8ba95321f27499039bc0
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-16 20:23:30 +0300

    feat: implement the logic of the work of Boots-Runners

---
+++ Vehicles/GroundVehicles/BootsRunners.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1.Vehicles.GroundVehicles
+{
+    internal class BootsRunners: GroundVehicle
+    {
+        private int maxTravelTime = 5;
+        private int restDuration = 1;
+        private int speed = 1;
+        private int distanceTraveled = 0;
+        private int timeTraveled = 0;
+
+        public override double DistanceTraveled
+        {
+            get { return distanceTraveled; }
+        }
+
+        public override int TimeTraveled
+        {
+            get { return timeTraveled; }
+            set { timeTraveled = value; }
+        }
+        public override void Move()
+        {
+            if (timeTraveled < maxTravelTime)
+            {
+                distanceTraveled += speed;
+            }
+            else if (timeTraveled >= maxTravelTime + restDuration)
+            {
+                timeTraveled = 0;
+            }
+            timeTraveled++;
+            Console.WriteLine($"Дистанция - {distanceTraveled.ToString()} Время{timeTraveled.ToString()}");
+            Console.WriteLine();
+
+        }
+        public override int Speed 
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public override int MaxTravelTime 
+        { 
+            set { maxTravelTime = value;  }
+            get { return maxTravelTime; }
+        }
+
+        public override int RestDuration
+        {
+            set { restDuration = value; }
+            get { return restDuration; }
+        }
+
+    }
+}
