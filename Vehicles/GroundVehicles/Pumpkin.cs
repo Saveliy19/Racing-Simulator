f88b0ab64f6f5a3409351bf74750bc1eab87d1f6
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-17 16:05:16 +0300

    feat: implement the logic of the work of Pumpkin

---
+++ Vehicles/GroundVehicles/Pumpkin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1.Vehicles.GroundVehicles
+{
+    internal class Pumpkin: GroundVehicle
+    {
+        protected int maxTravelTime = 15;
+        protected int restDuration = 4;
+        protected int maxSpeed = 15;
+        protected int speed = 15;
+
+        public override int DistanceTraveled => distanceTraveled;
+
+        protected override int Speed => speed;
+
+        protected override int MaxTravelTime => maxTravelTime;
+
+        protected override int RestDuration => restDuration;
+
+        public override void Move()
+        {
+            if (timeTraveled < maxTravelTime)
+            {
+                distanceTraveled += speed;
+                speed -= 1; //скорость уменьшается на каждом тике, затем восстанавливается после отдыха
+            }
+            else if (timeTraveled >= maxTravelTime + restDuration)
+            {
+                timeTraveled = 0;
+                speed = maxSpeed;
+            }
+            timeTraveled++;
+            Console.WriteLine($"Дистанция - {distanceTraveled.ToString()} Время{timeTraveled.ToString()}");
+            Console.WriteLine($"Скорость: {speed.ToString()}");
+            Console.WriteLine();
+
+        }
+    }
+}
