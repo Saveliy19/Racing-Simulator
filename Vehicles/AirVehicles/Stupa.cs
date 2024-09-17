f05c76314d497969b7daf41f68ba4394eb3a3ff8
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-17 14:56:44 +0300

    feat: implement the logic of the work of Stupa

---
+++ Vehicles/AirVehicles/Stupa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1.Vehicles.AirVehicles
+{
+    internal class Stupa: AirVehicle 
+    {
+        int accelerationCoefficient = 0;
+        int speed = 1; 
+        protected override int AccelerationCoefficient
+        {
+            get
+            {
+                return accelerationCoefficient;
+            }
+
+            set
+            {
+                accelerationCoefficient = value;
+            }
+        }
+
+        public override int DistanceTraveled 
+        {
+            get { return distanceTraveled;  }
+        }
+
+        public override void Move()
+        {
+            distanceTraveled = speed * timeTraveled + (accelerationCoefficient * timeTraveled * timeTraveled) / 2;
+            Console.WriteLine($"Дистанция ступы - {distanceTraveled.ToString()} Время ступы {timeTraveled.ToString()}");
+            Console.WriteLine();
+            accelerationCoefficient = distanceTraveled;
+            timeTraveled++;
+
+        }
+
+        protected override int Speed { get; set; }
+
+    }
+}
